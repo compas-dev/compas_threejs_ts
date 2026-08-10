c34562b6a62c89481acd18ff43c97fb7c13053c3
Gonzalo Casas <casas@arch.ethz.ch>
2026-08-10 18:34:39 +0200

    fix: reject non-renderable COMPAS objects

--- src/conversions/converter.ts
+++ src/conversions/converter.ts
@@ -8,6 +8,7 @@ import {
     Cylinder,
     Ellipse,
     Frame,
+    Graph,
     Hyperbola,
     Line,
     Mesh,
@@ -31,15 +32,65 @@ import {
     Vector,
 } from "@gramaziokohler/compas-pb-ts";
 
+import * as THREE from "three";
+
 import * as GEOCONV from "./geometry";
 import * as DATASTRUCTCONV from "./datastructures";
 
-export function convertToThreeJSGeometry(object: undefined) {
+const UNIMPLEMENTED_RENDERABLES = [
+    Arc,
+    Bezier,
+    Ellipse,
+    Hyperbola,
+    Parabola,
+    Polygon,
+];
+const NON_RENDERABLES = [
+    Graph,
+    Projection,
+    Quaternion,
+    Reflection,
+    Rotation,
+    Scale,
+    Shear,
+    Transformation,
+    Translation,
+];
+
+function objectType(object: unknown): string {
+    if (object === null) return "null";
+    if (object === undefined) return "undefined";
+    if (typeof object !== "object") return typeof object;
+    return object.constructor?.name || "object";
+}
+
+export class UnsupportedCompasObjectError extends TypeError {
+    readonly objectType: string;
+
+    constructor(object: unknown, reason = "is not supported by the viewer") {
+        const type = objectType(object);
+        super(`${type} ${reason}`);
+        this.name = "UnsupportedCompasObjectError";
+        this.objectType = type;
+    }
+}
+
+export function convertToThreeJSGeometry(object: unknown): THREE.Object3D {
+    if (UNIMPLEMENTED_RENDERABLES.some((Type) => object instanceof Type)) {
+        throw new UnsupportedCompasObjectError(
+            object,
+            "does not have an implemented renderer",
+        );
+    }
+
+    if (NON_RENDERABLES.some((Type) => object instanceof Type)) {
+        throw new UnsupportedCompasObjectError(
+            object,
+            "is data, not renderable scene geometry",
+        );
+    }
+
     switch (true) {
-        case object instanceof Arc:
-            return GEOCONV.arcToThreeJS(object);
-        case object instanceof Bezier:
-            return GEOCONV.bezierToThreeJS(object);
         case object instanceof Box:
             return GEOCONV.boxToThreeJS(object);
         case object instanceof Capsule:
@@ -50,46 +101,22 @@ export function convertToThreeJSGeometry(object: undefined) {
             return GEOCONV.coneToThreeJS(object, 64);
         case object instanceof Cylinder:
             return GEOCONV.cylinderToThreeJS(object, 64);
-        case object instanceof Ellipse:
-            return GEOCONV.ellipseToThreeJS(object, 64);
         case object instanceof Frame:
             return GEOCONV.frameToThreeJS(object);
-        case object instanceof Hyperbola:
-            return GEOCONV.hyperbolaToThreeJS(object);
         case object instanceof Line:
             return GEOCONV.lineToThreeJS(object);
-        case object instanceof Parabola:
-            return GEOCONV.parabolaToThreeJS(object);
         case object instanceof Plane:
             return GEOCONV.planeToThreeJS(object);
         case object instanceof Point:
             return GEOCONV.pointToThreeJS(object);
         case object instanceof Pointcloud:
             return GEOCONV.pointcloudToThreeJS(object);
-        case object instanceof Polygon:
-            return GEOCONV.polygonToThreeJS(object);
         case object instanceof Polyline:
             return GEOCONV.polylineToThreeJS(object);
-        case object instanceof Projection:
-            return GEOCONV.projectionToThreeJS(object);
-        case object instanceof Quaternion:
-            return GEOCONV.quaternionToThreeJS(object);
-        case object instanceof Reflection:
-            return GEOCONV.reflectionToThreeJS(object);
-        case object instanceof Rotation:
-            return GEOCONV.rotationToThreeJS(object);
-        case object instanceof Scale:
-            return GEOCONV.scaleToThreeJS(object);
-        case object instanceof Shear:
-            return GEOCONV.shearToThreeJS(object);
         case object instanceof Sphere:
             return GEOCONV.sphereToThreeJS(object);
         case object instanceof Torus:
             return GEOCONV.torusToThreeJS(object);
-        case object instanceof Transformation:
-            return GEOCONV.transformationToThreeJS(object);
-        case object instanceof Translation:
-            return GEOCONV.translationToThreeJS(object);
         case object instanceof Vector:
             return GEOCONV.vectorToThreeJS(object);
         case object instanceof Mesh:
@@ -97,4 +124,6 @@ export function convertToThreeJSGeometry(object: undefined) {
         case object instanceof Polyhedron:
             return DATASTRUCTCONV.polyhedronToThreeJS(object);
     }
+
+    throw new UnsupportedCompasObjectError(object);
 }
---
+++ tests/converter.test.ts
@@ -0,0 +1,58 @@
+import { Box, Quaternion } from "@gramaziokohler/compas-pb-ts";
+import * as THREE from "three";
+import { describe, expect, it } from "vitest";
+
+import {
+    convertToThreeJSGeometry,
+    UnsupportedCompasObjectError,
+} from "../src/conversions/converter";
+
+describe("convertToThreeJSGeometry", () => {
+    it("converts a box to renderable scene geometry", () => {
+        const box = new Box({
+            data: {
+                guid: "box-guid",
+                name: "Box",
+                frame: {
+                    point: { guid: "", name: "", x: 4, y: 5, z: 6 },
+                    xaxis: { guid: "", name: "", x: 1, y: 0, z: 0 },
+                    yaxis: { guid: "", name: "", x: 0, y: 1, z: 0 },
+                },
+                xsize: 1,
+                ysize: 2,
+                zsize: 3,
+            },
+        });
+
+        const converted = convertToThreeJSGeometry(box);
+
+        expect(converted).toBeInstanceOf(THREE.Mesh);
+        expect(converted.position.toArray()).toEqual([4, 5, 6]);
+    });
+
+    it("rejects mathematical data instead of adding it to a scene", () => {
+        const quaternion = new Quaternion({
+            data: {
+                guid: "quaternion-guid",
+                name: "Quaternion",
+                w: 0.5,
+                x: 0.5,
+                y: 0.5,
+                z: 0.5,
+            },
+        });
+
+        expect(() => convertToThreeJSGeometry(quaternion)).toThrowError(
+            new UnsupportedCompasObjectError(
+                quaternion,
+                "is data, not renderable scene geometry",
+            ),
+        );
+    });
+
+    it("rejects unknown objects with a typed error", () => {
+        expect(() => convertToThreeJSGeometry({})).toThrow(
+            UnsupportedCompasObjectError,
+        );
+    });
+});
